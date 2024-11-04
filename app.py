a8b3b0349c457e5cf981361c2ede3f7d91e17385
mart <martindore21@gmail.com>
2024-11-04 17:08:19 +0100

    modification on form and data extraction

--- app.py
+++ app.py
@@ -1,32 +1,45 @@
-import numpy as np
 from flask import Flask, request, jsonify, render_template
+import numpy as np
 import joblib
 
 app = Flask(__name__)
 model = joblib.load('model.pkl')
-print(model)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
+    # Extraction des valeurs numériques
+    cylinders = float(request.form['cylinders'])
+    displacement = float(request.form['displacement'])
+    horsepower = float(request.form['horsepower'])
+    weight = float(request.form['weight'])
+    acceleration = float(request.form['acceleration'])
+    model_year = float(request.form['model year'])
+
+    # Récupération des valeurs d'origine avec une valeur par défaut de 0
+    origin_asia = int(request.form.get('origin_Asia', 0))
+    origin_europe = int(request.form.get('origin_Europe', 0))
+    origin_usa = int(request.form.get('origin_USA', 0))
+
+    # Création de la liste finale des caractéristiques pour la prédiction
+    final_features = np.array([cylinders, displacement, horsepower, weight, acceleration, model_year, origin_asia, origin_europe, origin_usa]).reshape(1, -1)
+
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
 
     return render_template('index.html', prediction_text='Prediction MPG : {}'.format(output))
 
-@app.route('/predict_api',methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
     '''
-    For direct API calls trought request
+    For direct API calls through request
     '''
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
